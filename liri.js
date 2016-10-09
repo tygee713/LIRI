cb77976db2e9d570b2b5f9298296de26d8510495
Tyler Geerdts <tgeerdts@Tylers-MacBook-Pro.local>
2016-10-09 12:11:43 -0400

    Finished Spotify and OMDb functionality

--- liri.js
+++ liri.js
@@ -34,15 +34,33 @@ switch (command) {
         console.log("song not found");
       }
       else {
-        console.log(data.tracks.items[0].artists[0].name);
-        console.log(data.tracks.items[0].name);
-        console.log(data.tracks.items[0].external_urls.spotify);
-        console.log(data.tracks.items[0].album.name);
+        for (i=0;i<data.tracks.items[0].artists.length;i++) {
+          console.log("Artist: " + data.tracks.items[0].artists[i].name);
+        }
+        console.log("Song Name: " + data.tracks.items[0].name);
+        console.log("Spotify URL: " + data.tracks.items[0].external_urls.spotify);
+        console.log("Album Name: " + data.tracks.items[0].album.name);
       }
     });
     break;
   case 'movie-this':
-
+    if (name == undefined || name == "") {
+          name = 'Mr. Nobody';
+        }
+    var queryUrl = 'http://www.omdbapi.com/?t=' + name +'&y=&plot=short&r=json&tomatoes=true';
+    request(queryUrl, function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log("Title: " + JSON.parse(body).Title);
+        console.log("Year: " + JSON.parse(body).Year);
+        console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+        console.log("Country: " + JSON.parse(body).Country);
+        console.log("Language: " + JSON.parse(body).Language);
+        console.log("Plot: " + JSON.parse(body).Plot);
+        console.log("Actors: " + JSON.parse(body).Actors);
+        console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
+        console.log("URL: " + JSON.parse(body).tomatoURL);
+      }
+    });
     break;
   case 'do-what-it-says':
 
@@ -50,4 +68,3 @@ switch (command) {
   default:
     console.log("Not a valid selection");
 }
-
